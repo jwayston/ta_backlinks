b8afa79b138028080b8cf0388c00b8ca3564f15e
J Wayston <177466602+jwayston@users.noreply.github.com>
2026-02-24 22:48:49 +0200

    Backlink description extraction changes & refactoring
    Backlink description is primarily extracted from the filename and
    secondarily from the first H1 header found.

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 This plugin shows the backlinks of the current note in a separate window.
 
-The plugin aims to tackle the challenge of understanding backlinked notes when
-notes' filenames are pure Zettel IDs.
+The plugin aims to tackle the challenge of understanding backlinked notes when notes' filenames are pure UIDs. The traditional file naming convention is supported however.
+
+The description for the backlink is extracted primarily from the filename. If the filename is just the UID, it pulls the first H1 header.
 
--- com.idonthavedomain.backlinks.thearchiveplugin/main.js
+++ com.idonthavedomain.backlinks.thearchiveplugin/main.js
@@ -7,55 +7,66 @@ Author: JW
 
 "use strict";
 
-const LINEBR = "\n";
-const idRegex = new RegExp(/^([0-9]{10,12}|^.*?)(\s|$)/);
-const noteHeaderRegexp = new RegExp("(?<=^|\\n)# (.+)\\n");
+const currentNote = input.notes.selected[0].filename;
+const regexId = new RegExp(/^([0-9]{8,14}|^.*?)(\s.*|$)/);
+const regexNoteHeader = new RegExp("(?<=^|\\n)# (.+)\\n");
+const currentNoteId = getCurrentNoteId();
+
 
 function extractHeader(noteContent)
 {
-    const contentMatch = noteContent.match(noteHeaderRegexp);
-    if (contentMatch)
-    {
-        return contentMatch[1];
-    }
-    else
-    {
-        return "<No detectable H1 header found>";
-    }
+    const contentMatch = noteContent.match(regexNoteHeader);
+    return contentMatch ? contentMatch[1] : null;
 }
 
-const currentNote = input.notes.selected[0].filename;
-const idMatch = currentNote.match(idRegex);
-
-if (!idMatch)
+function getCurrentNoteId()
 {
-	cancel("Could not parse ID from the current note's filename!");
+    const matchId = currentNote.match(regexId);
+
+    if (!matchId)
+        cancel("Could not parse ID from the current note's filename!");
+    return matchId[1];
+}
+
+function parseBacklinks()
+{
+    let outputText = "";
+    const searchResults = app.search(currentNoteId);
+
+    let idx = 0;
+    for(const note of searchResults.results)
+    {
+        let noteId = note.filename;
+        let noteFileHeader;
+        let noteHeader = extractHeader(note.content);;
+
+        // Don't include current file in the results
+        if (note.filename == currentNote)
+            continue;
+
+        const matchNoteId = note.filename.match(regexId);
+
+        if (matchNoteId)
+        {
+            noteId = matchNoteId[1];
+            noteFileHeader = matchNoteId[2].replace(" ", "");
+        }
+
+        if (noteFileHeader)
+            noteHeader = noteFileHeader;
+        else
+            if (noteHeader === null)
+                noteHeader = "<No header or file description available>";
+
+        const textLine = `${idx+1}. ${noteHeader} [[${noteId}]]\n`;
+
+        outputText += textLine;
+        idx++;
+    }
+
+    return outputText;
 }
 
 
-let outputText = "";
-const currentNoteId = idMatch[1];
-const searchResults = app.search(currentNoteId);
+output.display.content = `# Backlinks for "${currentNote}"\n\n${parseBacklinks()}`;
 
-let idx = 1;
-searchResults.results.forEach(elem => 
-{
-	// Don't include current file in the results
-	if (elem.filename == currentNote) return;
-
-	let noteId = elem.filename;
-	const noteIdMatch = elem.filename.match(idRegex);
-	
-	if (noteIdMatch)
-	{
-		noteId = noteIdMatch[1];
-	}
-
-	const noteHeader = extractHeader(elem.content);
-	const textLine = `${idx}. ${noteHeader} [[${noteId}]]\n`;
-
-	outputText += textLine;
-	idx++;
-});
-
-output.display.content = `# Backlinks for "${currentNoteId}"\n\n${outputText}`;
